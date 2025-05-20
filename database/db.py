6d9dd8d80fb2b72ff535f36da0abaece8866288b
lekfinha <franco.valdes@ug.uchile.cl>
2025-05-20 17:13:07 -0400

    falta menos

--- database/db.py
+++ database/db.py
@@ -2,8 +2,8 @@ import pymysql
 import os
 
 DB_NAME = "tarea2"
-DB_USERNAME = "cc5002"
-DB_PASSWORD = "programacionweb"
+DB_USERNAME = "user"
+DB_PASSWORD = "pass"
 DB_HOST = "localhost"
 DB_PORT = 3306
 DB_CHARSET = "utf8"
